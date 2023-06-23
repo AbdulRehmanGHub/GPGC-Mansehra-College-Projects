59e45ab2364c72eea5d6473f20641f3fb1004ab8
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2023-06-23 20:30:22 +0500

    Even Number

---
+++ Second Year/Even Number.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+
+{
+	int x;
+
+	cout <<"Please enter a number"<<endl;
+
+	cin >>x;
+
+	if (x%2==0);
+
+	{
+		cout <<"Number is Even"<<endl;
+	}
+	
+	getch();
+	
+    return 0;
+}
+
+
+
+

0d735529003c426dd88e64a72ef913042568bf26
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2023-06-24 10:18:32 +0500

    If Statement

---
+++ Second Year/If Statement.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+
+ {
+ 	
+	 // Declare an integer "sal" for salary and an integer "bon" for bonus.
+	 
+	 int sal,bon;
+ 	 
+ 	 // Display a string "Enter your salary".
+ 	 
+ 	 cout <<"Enter your salary"<<endl;
+ 	 
+	  // Get value of "sal" from user.
+ 	 
+ 	 cin >>sal;
+
+// Apply condition for 10% bonous.
+ 	 
+ 	 if (sal>10000)
+{   
+
+    // Use bonus formula.
+    
+	bon = (sal*10)/100;
+	
+	// Now display bonus amount.
+	
+	cout <<"Your bonus is "<<bon<<endl;
+	
+	// Add bonus to salary.
+	sal = sal+bon;     
+ } 
+ 
+ // Display Salary after adding 10% bonus.
+ 
+ cout <<"Total Salary is  "<<sal<<endl;
+ 
+ 	
+getch();
+
+return 0;
+ 	
+ }
